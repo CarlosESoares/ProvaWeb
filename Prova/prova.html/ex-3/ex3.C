f1065391d764e74bf973f0883fc858c5c14d9701
unknown <carlos.s2006@aluno.ifsc.edu.br>
2024-10-25 10:25:09 -0300

    Prova

---
+++ Prova/prova.html/ex-3/ex3.C
@@ -0,0 +1,24 @@
+Para posicionar ultilizamos a tag posicion para modificar 
+Ex:
+
+
+   .container {
+            position: relative;
+            width: 100%;
+            height: 100vh;
+        }
+        .box {
+            position: absolute;
+            width: 100px;
+            height: 100px;
+            color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-weight: bold;
+        }
+        .topo-esquerdo{
+            top: 10px;
+            left: 10px;
+            background-color: red;
+        }
